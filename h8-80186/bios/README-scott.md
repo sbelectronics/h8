fa424e66836bff939326c37a3e3600ffac27f684
Scott Baker <smbaker@gmail.com>
2023-12-16 19:11:46 -0800

    bios WIP

---
+++ h8-80186/bios/README-scott.md
@@ -0,0 +1,1 @@
+BIOS source and build are in /home/smbaker/projects/pi/sbc188/sbc188-bios-smbaker
---
+++ h8-80186/bios/split.py
@@ -0,0 +1,17 @@
+import os
+
+data = open("/home/smbaker/projects/pi/sbc188/sbc188-bios-smbaker/bios064.bin","rb").read()
+
+flo = open("biosl.bin","wb")
+fhi = open("biosh.bin","wb")
+
+for i in range(0,4):
+  i=0
+  for x in data:
+    if(i%2)==0:
+      flo.write(bytes([x]))
+    else:
+      fhi.write(bytes([x]))
+    i+=1
+
+
